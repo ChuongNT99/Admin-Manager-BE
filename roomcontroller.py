02f89a7eeb8aa0e6cbdd740c75f91e0249c08949
ChuongNT <chuongnt_tts@rikkeisoft.com>
2023-10-29 14:56:43 +0700

    update error exists

--- roomcontroller.py
+++ roomcontroller.py
@@ -3,84 +3,106 @@ import mysql.connector
 
 app = Flask(__name__)
 
-# Cấu hình kết nối cơ sở dữ liệu
 db_config = {
-    'host': 'localhost',
-    'user': 'root',
-    'password': '001122',
-    'database': 'admin_manager_db',
+    "host": "localhost",
+    "user": "root",
+    "password": "001122",
+    "database": "admin_manager_db",
 }
 
 def create_db_connection():
     return mysql.connector.connect(**db_config)
 
-@app.route('/rooms', methods=['GET'])
+@app.route("/rooms", methods=["GET"])
 def get_rooms():
     try:
         conn = create_db_connection()
         cursor = conn.cursor(dictionary=True)
         cursor.execute("SELECT * FROM room_meeting")
         rows = cursor.fetchall()
-        return jsonify({'rooms': rows})
+        return jsonify({"rooms": rows})
     except Exception as e:
-        return jsonify({'error': 'Internal Server Error'}), 500
+        return jsonify({"error": "Internal Server Error"}), 500
     finally:
         cursor.close()
         conn.close()
 
-@app.route('/rooms', methods=['POST'])
+@app.route("/rooms", methods=["POST"])
 def create_room():
     try:
         data = request.get_json()
-        room_name = data.get('room_name')
-        status = data.get('status')
+        room_name = data.get("room_name")
+        status = data.get("status")
 
         conn = create_db_connection()
         cursor = conn.cursor()
-        cursor.execute("INSERT INTO room_meeting (room_name, status) VALUES (%s, %s)", (room_name, status))
+        cursor.execute(
+            "SELECT room_name FROM room_meeting WHERE room_name = %s", (room_name,)
+        )
+        existing_room = cursor.fetchone()
+
+        if existing_room:
+            return jsonify({"error": "Room already exists"}), 400
+
+        cursor.execute(
+            "INSERT INTO room_meeting (room_name, status) VALUES (%s, %s)",
+            (room_name, status),
+        )
         conn.commit()
-        return jsonify({'message': 'Room created successfully'})
+        return jsonify({"message": "Room created successfully"})
     except Exception as e:
-        return jsonify({'error': 'Internal Server Error'}), 500
+        return jsonify({"error": "Internal Server Error"}), 500
     finally:
         cursor.close()
         conn.close()
 
-@app.route('/rooms/<int:room_id>', methods=['PUT'])
+@app.route("/rooms/<int:room_id>", methods=["PUT"])
 def update_room(room_id):
     try:
         data = request.get_json()
-        room_name = data.get('room_name')
-        status = data.get('status')
+        room_name = data.get("room_name")
+        status = data.get("status")
 
         conn = create_db_connection()
         cursor = conn.cursor()
-        cursor.execute("UPDATE room_meeting SET room_name=%s, status=%s WHERE room_id=%s", (room_name, status, room_id))
+        cursor.execute(
+            "SELECT room_name FROM room_meeting WHERE room_id != %s AND room_name = %s",
+            (room_id, room_name),
+        )
+        existing_room = cursor.fetchone()
+
+        if existing_room:
+            return jsonify({"error": "Room name already exists"}), 400
+
+        cursor.execute(
+            "UPDATE room_meeting SET room_name=%s, status=%s WHERE room_id=%s",
+            (room_name, status, room_id),
+        )
         conn.commit()
-        return jsonify({'message': 'Room updated successfully'})
+        return jsonify({"message": "Room updated successfully"})
     except Exception as e:
-        return jsonify({'error': 'Internal Server Error'}), 500
+        return jsonify({"error": "Internal Server Error"}), 500
     finally:
         cursor.close()
         conn.close()
 
-@app.route('/rooms/<int:room_id>', methods=['DELETE'])
+@app.route("/rooms/<int:room_id>", methods=["DELETE"])
 def delete_room(room_id):
     try:
         conn = create_db_connection()
         cursor = conn.cursor()
         cursor.execute("DELETE FROM room_meeting WHERE room_id=%s", (room_id,))
         conn.commit()
-        return jsonify({'message': 'Room deleted successfully'})
+        return jsonify({"message": "Room deleted successfully"})
     except Exception as e:
-        return jsonify({'error': 'Internal Server Error'}), 500
+        return jsonify({"error": "Internal Server Error"}), 500
     finally:
         cursor.close()
         conn.close()
 
 @app.errorhandler(404)
 def not_found(e):
-    return jsonify({'error': 'Not Found'}), 404
+    return jsonify({"error": "Not Found"}), 404
 
 if __name__ == "__main":
     app.run(debug=True)
